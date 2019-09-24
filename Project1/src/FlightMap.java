4cddb52353d055beee72d03d9026a70c7156b989
Hekmat <Matt@Hekmats-MacBook-Pro.local>
2019-09-23 18:37:44 -0700

    SearchMap still needs clean up and comments. XML is done

--- Project1/src/FlightMap.java
+++ Project1/src/FlightMap.java
@@ -4,12 +4,23 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class FlightMap {
+	//Storing the cities
 	public LinkedList<String> cities;
+	//Adjacent matrix for placing edges
 	public int[][] adj_matrix;
+	//Matrix for cost
 	public int[][] cost;
+	//Array used to check if a citiy was visited
+	//when finding the routes
 	public boolean[] visited;
+	
+	
+	String[] routes;
+	int[] costs;
+	
 
 	String temp2 = new String();
+
 	//Constructor
 	public FlightMap() {
 		cities = new LinkedList<String>();
@@ -21,6 +32,8 @@ public class FlightMap {
 	}
 	
 	
+	
+	//Function that stores the cities in LinkedList
 	public void readCities(String content) {
 		//Get Cities
 		String[] temp = content.split(" ");
@@ -37,6 +50,7 @@ public class FlightMap {
 		}			
 	}
 	
+	//Places the edges 
 	public void placeEdges() {
 		adj_matrix = new int[cities.size()][cities.size()];
 		cost = new int[cities.size()][cities.size()];
@@ -52,40 +66,39 @@ public class FlightMap {
 			int index1 = cities.indexOf(c[0]);
 			int index2 = cities.indexOf(c[1]);
 			adj_matrix[index1][index2] = 1;
-			cost[index1][index2] = 1;
+			cost[index1][index2] = Integer.parseInt(c[2]);
 		}
 		/*
 		for(int i = 0; i < cities.size(); i++)
 			System.out.print(cities.get(i) + " ");
 		System.out.println();
-		for(int i = 0; i < adj_matrix.length; i++) {
-			for(int j = 0; j < adj_matrix.length; j++) {
-				System.out.print(adj_matrix[i][j] + " ");
+		for(int i = 0; i < cost.length; i++) {
+			for(int j = 0; j < cost.length; j++) {
+				System.out.print(cost[i][j] + " ");
 			}
 			System.out.print("\n");
 		}
 		*/
 		
 	}
+	
+	//Computes the routes
 	public void computeRoutes() {
-		for(int i = 0; i < adj_matrix.length; i++) {
-			for(int j = 0; j < adj_matrix.length; j++) {
-				if(adj_matrix[i][j] == 1) {
-					//System.out.println(cities.get(i) + "->" + cities.get(j));
-				}
-			}
-		}
-		visited = new boolean[cities.size()];
-	    int[] path = new int[cities.size()];
-	    findPath(0, 5,path,0);
-		//Queue<Integer> path = new ArrayDeque<>();
-		//System.out.println(isConnected(0,2,path) + " " + path);
-		
-		
-
-
-
-	}
+		//Path
+	    int[] temp;
+	    routes = new String[cities.size()];
+	    
+	    for(int i = 1; i < cities.size(); i++) {
+	    	//Reset the temp path
+	    	temp = new int[cities.size()];
+	    	//Reset the visited nodes value
+			visited = new boolean[cities.size()];	
+	    	findPath(0, i, temp, 0);
+	    }
+	    computeCosts();	
+	    }
+	
+	
 	//Backtracking to find a path
 	public void findPath(int src, int target, int path[], int ctr) {
 		visited[src] = true;
@@ -93,8 +106,13 @@ public class FlightMap {
 		ctr++;
 		//Path found
 		if(src == target) {
+			routes[target] = new String();
 			for(int i = 0; i < ctr; i++) {
-				System.out.println(cities.get(path[i]));
+				if(i == 0) {
+					routes[target] = cities.get(path[i]);
+				}else {
+					routes[target] = routes[target] + "," + cities.get(path[i]);
+				}
 			}
 			return;
 		}else {
@@ -108,5 +126,39 @@ public class FlightMap {
 		}
 		ctr--;
 		visited[src] = false;
+	}
+	public void computeCosts() {
+		this.costs = new int[cities.size()];
+		for(int i = 1; i < routes.length; i++) {
+			if(routes[i] != null) {
+				String[] cities = routes[i].split(",");
+				int cost = 0;
+				for(int j = 0; j < cities.length; j++) {
+					if(j  == (cities.length - 1)) {
+						continue;
+					}else {
+						int index1 = this.cities.indexOf(cities[j]);
+						int index2 = this.cities.indexOf(cities[j + 1]);
+						cost = cost + this.cost[index1][index2];
+						this.costs[i] = cost;
+					}
+				}
+			}
+		}
+	}
+	
+	public int getRouteCosts(int index) {
+		return this.costs[index];
+	}
+	//Get method to get all the routes computed
+	public String[] getRoutes() {
+		return this.routes;
+	}
+	//Get method to get the city based on the index value in the LL
+	public String getCity(int index) {
+		return this.cities.get(index);
+	}
+	public int howManycities() {
+		return this.cities.size();
 	}
 }
--- Project1/src/SearchMap.java
+++ Project1/src/SearchMap.java
@@ -1,28 +1,60 @@
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class SearchMap {
-
+	
+	FlightMap obj;
 	public static void main(String args[]) {
-		//SearchMap s = new SearchMap();
-		//s.readFile();
-		System.out.println("hello my friend");
+		SearchMap s = new SearchMap();
+		s.readFile();
+		s.computeRoutes();
+		s.writeToFile();
 
+	}
+	
+	public SearchMap() {
+		obj = new FlightMap();
 	}
 	public void readFile() {
 		String fileName = "file.txt";
 		try {
-
 			FileReader fr = new FileReader(fileName);
 			Scanner s = new Scanner(fr);
-			FlightMap obj = new FlightMap();
 			while(s.hasNextLine()) {
 				obj.readCities(s.nextLine());			
 			}
 			obj.placeEdges();
-			obj.computeRoutes();
 		}catch(Exception e) {
-			System.out.println("File Does not Exists");
+			System.out.println(e.getMessage());
 		}
+	}
+	public void computeRoutes() {
+		obj.computeRoutes();
+	}
+	public void writeToFile() {
+		
+		//Write to a file
+		try {
+			String[] routesComputed = obj.getRoutes();
+		    BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+		    writer.write("Destination       Flight Route from P       Total Cost");
+		    for(int i = 0; i < routesComputed.length; i++) {
+				if(routesComputed[i] != null) {
+					writer.newLine();
+					String des = obj.getCity(i);
+					String route = routesComputed[i];
+					int routeCost = obj.getRouteCosts(i);
+					writer.write(des + "       " + route + "       " + routeCost);
+				}
+			}
+		     
+		    writer.close();
+		}catch(IOException o) {
+			System.out.println(o.getMessage());
+		}
+	
 	}
 }
